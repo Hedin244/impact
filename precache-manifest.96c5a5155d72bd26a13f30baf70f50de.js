0fe545308c37dd396eb3f95171bb5fd588affde4
karol <k.hinc@artgeist.com>
2019-09-16 13:03:52 +0200

    Updates

---
+++ precache-manifest.96c5a5155d72bd26a13f30baf70f50de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f89a8d2573fb6ff90ad14a11c285ff",
+    "url": "/impact/index.html"
+  },
+  {
+    "revision": "8c88f3385bf0df10ef37",
+    "url": "/impact/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4cc816e0374e15aa43b3",
+    "url": "/impact/static/js/2.bd166724.chunk.js"
+  },
+  {
+    "revision": "8c88f3385bf0df10ef37",
+    "url": "/impact/static/js/main.f98962ee.chunk.js"
+  },
+  {
+    "revision": "37835aff76efba07942d",
+    "url": "/impact/static/js/runtime~main.50627d02.js"
+  }
+]);
