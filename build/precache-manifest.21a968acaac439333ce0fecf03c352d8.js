6351df0eb31c70bda429053d91887a00f5dcecae
karol <k.hinc@artgeist.com>
2019-09-16 12:52:30 +0200

    deploy v3

---
+++ build/precache-manifest.21a968acaac439333ce0fecf03c352d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184fbadf48faf6756608f3ccc585bfd0",
+    "url": "/impact-frontend-app/index.html"
+  },
+  {
+    "revision": "6ae852bfef8777cc004f",
+    "url": "/impact-frontend-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4cc816e0374e15aa43b3",
+    "url": "/impact-frontend-app/static/js/2.bd166724.chunk.js"
+  },
+  {
+    "revision": "6ae852bfef8777cc004f",
+    "url": "/impact-frontend-app/static/js/main.2f7db08d.chunk.js"
+  },
+  {
+    "revision": "c769b03f3ce529d7667e",
+    "url": "/impact-frontend-app/static/js/runtime~main.aa222c9c.js"
+  }
+]);
